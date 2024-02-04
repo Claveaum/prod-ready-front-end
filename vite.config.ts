de8dd95fcc3bd5d6e33b3c4ed8ceb75f0cb22c0a
Maxime Claveau <maxime.claveau@maif.fr>
2024-02-04 12:03:43 +0100

    allow launch in src or correction folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import {configDefaults} from "vitest/config";
 
 // https://vitejs.dev/config/
-const folder: string = 'src';
-export default defineConfig({
+export default({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  const folder: string = env.VITE_APP_FOLDER;
+  return defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
-    environment : 'jsdom',
-    setupFiles : `${folder}/setupTests.ts`,
+    environment: 'jsdom',
+    setupFiles: `${folder}/setupTests.ts`,
     coverage: {
       provider: "v8",
       enabled: true,
       reporter: ['text', 'html', 'lcov'],
       include: [`${folder}/*`],
-      exclude: [...configDefaults.exclude, `${folder}/main.tsx`,`${folder}/setupTests.ts`, "**/**.d.ts**"],
-      extension: ["ts","tsx"]
+      exclude: [...configDefaults.exclude, `${folder}/main.tsx`, `${folder}/setupTests.ts`, "**/**.d.ts**"],
+      extension: ["ts", "tsx"]
     },
     outputFile: 'reports/test-report.xml',
-    exclude: [...configDefaults.exclude, '**/build/**','correction/*', "cours/*"],
+    exclude: [...configDefaults.exclude, '**/build/**', 'correction/*', "cours/*"],
   }
-})
+})}
